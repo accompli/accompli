80a105c905485c5901df05f1528dcdccb32cef95
Niels Nijens <nijens.niels@gmail.com>
2015-03-22 23:17:05 +0100

    Added Accompli class

---
+++ src/Accompli.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Accompli;
+
+/**
+ * Accompli
+ *
+ * @author  Niels Nijens <nijens.niels@gmail.com>
+ * @package Accompli
+ * */
+class Accompli
+{
+    /**
+     * The Accompli CLI text logo
+     *
+     * @var string
+     **/
+    const LOGO = "
+     _                                 _ _
+    / \   ___ ___ ___  _ __ ___  _ __ | (_)
+   / _ \ / __/ __/ _ \| '_ ` _ \| '_ \| | |
+  / ___ \ (_| (_| (_) | | | | | | |_) | | |
+ /_/   \_\___\___\___/|_| |_| |_| .__/|_|_|
+ C'est fini. Accompli!          |_|
+";
+
+    /**
+     * The Accompli CLI slogan text
+     *
+     * @var string
+     **/
+    const SLOGAN = "C'est fini. Accompli!";
+
+    /**
+     * The Accompli version
+     *
+     * @var string
+     **/
+    const VERSION = "0.1";
+}
