1bc5648ad4a2da9ef12ae87df7298c160a0a6f9d
Niels Nijens <nijens.niels@gmail.com>
2015-03-25 22:56:14 +0100

    Added deployment failed events as defined in the new process in accompli/accompli#4

---
+++ src/Event/DeploymentFailedEvent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Accompli\Event;
+
+/**
+ * DeploymentFailedEvent
+ *
+ * @author  Niels Nijens <nijens.niels@gmail.com>
+ * @package Accompli\Event
+ **/
+class DeploymentFailedEvent extends AbstractDeploymentEvent
+{
+
+}
---
+++ src/Event/RollbackDeploymentFailedEvent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Accompli\Event;
+
+/**
+ * RollbackDeploymentFailedEvent
+ *
+ * @author  Niels Nijens <nijens.niels@gmail.com>
+ * @package Accompli\Event
+ **/
+class RollbackDeploymentFailedEvent extends AbstractDeploymentEvent
+{
+
+}
