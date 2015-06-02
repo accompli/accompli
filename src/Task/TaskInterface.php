3161eee36eedf161fea320f84ecbe3812c3220ba
Niels Nijens <nijens.niels@gmail.com>
2015-06-02 22:33:16 +0200

    Added TaskInterface

---
+++ src/Task/TaskInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Accompli\Task;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * TaskInterface
+ *
+ * @author  Niels Nijens <nijens.niels@gmail.com>
+ * @package Accompli\Task
+ */
+interface TaskInterface extends EventSubscriberInterface
+{
+
+}
