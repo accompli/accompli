bd6bd706cc6a9955e3e3e3aac1ea8918d242cf7e
Niels Nijens <nijens.niels@gmail.com>
2015-07-24 20:47:38 +0200

    Added GatherFactsEvent

--- src/AccompliEvents.php
+++ src/AccompliEvents.php
@@ -18,7 +18,7 @@ final class AccompliEvents
      *
      * @var string
      **/
-    const DEPLOY_DEPLOYMENT = "accompli.deploy_deployment";
+    const DEPLOY_DEPLOYMENT = 'accompli.deploy_deployment';
 
     /**
      * The DEPLOYMENT_COMPLETE event is dispatched when a Release succesfully deployed.
@@ -28,7 +28,7 @@ final class AccompliEvents
      *
      * @var string
      **/
-    const DEPLOYMENT_COMPLETE = "accompli.deployment_complete";
+    const DEPLOYMENT_COMPLETE = 'accompli.deployment_complete';
 
     /**
      * The DEPLOYMENT_FAILED event is dispatched when deployment of a Release has failed.
@@ -38,7 +38,17 @@ final class AccompliEvents
      *
      * @var string
      **/
-    const DEPLOYMENT_FAILED = "accompli.deployment_failed";
+    const DEPLOYMENT_FAILED = 'accompli.deployment_failed';
+
+    /**
+     * The GATHER_FACTS event is dispatched when information from a host is required.
+     *
+     * The event listener receives an
+     * Accompli\Event\GatherFactsEvent instance.
+     *
+     * @var string
+     **/
+    const GATHER_FACTS = 'accompli.gather_facts';
 
     /**
      * The INSTALL_RELEASE event is dispatched when a Release requires installation.
@@ -48,7 +58,7 @@ final class AccompliEvents
      *
      * @var string
      **/
-    const INSTALL_RELEASE = "accompli.install_release";
+    const INSTALL_RELEASE = 'accompli.install_release';
 
     /**
      * The PREPARE_DEPLOYMENT event is dispatched when a Release is prepared for deployment.
@@ -58,7 +68,7 @@ final class AccompliEvents
      *
      * @var string
      **/
-    const PREPARE_DEPLOYMENT = "accompli.prepare_deployment";
+    const PREPARE_DEPLOYMENT = 'accompli.prepare_deployment';
 
     /**
      * The PREPARE_RELEASE event is dispatched when a Release is being prepared.
@@ -68,7 +78,7 @@ final class AccompliEvents
      *
      * @var string
      **/
-    const PREPARE_RELEASE = "accompli.prepare_release";
+    const PREPARE_RELEASE = 'accompli.prepare_release';
 
     /**
      * The PREPARE_WORKSPACE event is dispatched when a server or local path is being prepared for deployments.
@@ -78,7 +88,7 @@ final class AccompliEvents
      *
      * @var string
      **/
-    const PREPARE_WORKSPACE = "accompli.prepare_workspace";
+    const PREPARE_WORKSPACE = 'accompli.prepare_workspace';
 
     /**
      * The ROLLBACK_DEPLOYMENT event is dispatched when a previous Release is being deployed.
@@ -88,7 +98,7 @@ final class AccompliEvents
      *
      * @var string
      **/
-    const ROLLBACK_DEPLOYMENT = "accompli.rollback_deployment";
+    const ROLLBACK_DEPLOYMENT = 'accompli.rollback_deployment';
 
     /**
      * The ROLLBACK_DEPLOYMENT_COMPLETE event is dispatched when a previous Release is successfully deployed.
@@ -98,7 +108,7 @@ final class AccompliEvents
      *
      * @var string
      **/
-    const ROLLBACK_DEPLOYMENT_COMPLETE = "accompli.rollback_deployment_complete";
+    const ROLLBACK_DEPLOYMENT_COMPLETE = 'accompli.rollback_deployment_complete';
 
     /**
      * The ROLLBACK_DEPLOYMENT_FAILED event is dispatched when deployment of a previous Release has failed.
@@ -108,5 +118,5 @@ final class AccompliEvents
      *
      * @var string
      **/
-    const ROLLBACK_DEPLOYMENT_FAILED = "accompli.rollback_deployment_failed";
+    const ROLLBACK_DEPLOYMENT_FAILED = 'accompli.rollback_deployment_failed';
 }
---
+++ src/Event/GatherFactsEvent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Accompli\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * GatherFactsEvent
+ *
+ * @author  Niels Nijens <nijens.niels@gmail.com>
+ * @package Accompli\Event
+ **/
+class GatherFactsEvent extends Event
+{
+}
