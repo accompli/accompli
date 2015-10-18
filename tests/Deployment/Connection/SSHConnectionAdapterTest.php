ba42b9266d52bfa9093aedec533f422497764d9f
Niels Nijens <nijens.niels@gmail.com>
2015-10-18 18:34:26 +0200

    Extended SSHConnectionAdapterTest from ConnectedConnectionAdapterTestCase

--- tests/Deployment/Connection/SSHConnectionAdapterTest.php
+++ tests/Deployment/Connection/SSHConnectionAdapterTest.php
@@ -3,14 +3,14 @@
 namespace Accompli\Test;
 
 use Accompli\Deployment\Connection\SSHConnectionAdapter;
-use Accompli\Test\Deployment\Connection\ConnectionAdapterTestCase;
+use Accompli\Test\Deployment\Connection\ConnectedConnectionAdapterTestCase;
 
 /**
  * SSHConnectionAdapterTest.
  *
  * @author Niels Nijens <nijens.niels@gmail.com>
  */
-class SSHConnectionAdapterTest extends ConnectionAdapterTestCase
+class SSHConnectionAdapterTest extends ConnectedConnectionAdapterTestCase
 {
     /**
      * {@inheritdoc}
