e4b0212c10c989722567370abdbd43ad26d75375
Niels Nijens <nijens.niels@gmail.com>
2015-10-30 16:49:53 +0100

    Added AbstractDeploymentStrategy with service awareness

---
+++ src/Deployment/Strategy/AbstractDeploymentStrategy.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Accompli\Deployment\Strategy;
+
+use Accompli\Configuration\ConfigurationInterface;
+use Accompli\DependencyInjection\ConfigurationAwareInterface;
+use Accompli\DependencyInjection\EventDispatcherAwareInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * AbstractDeploymentStrategy.
+ *
+ * @author Niels Nijens <niels@connectholland.nl>
+ */
+abstract class AbstractDeploymentStrategy implements DeploymentStrategyInterface, ConfigurationAwareInterface, EventDispatcherAwareInterface
+{
+    /**
+     * The configuration instance.
+     *
+     * @var ConfigurationInterface
+     */
+    protected $configuration;
+
+    /**
+     * The event dispatcher instance.
+     *
+     * @var EventDispatcherInterface
+     */
+    protected $eventDispatcher;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setConfiguration(ConfigurationInterface $configuration)
+    {
+        $this->configuration = $configuration;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setEventDispatcher(EventDispatcherInterface $eventDispatcher)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deploy($version, $stage = null)
+    {
+    }
+}
