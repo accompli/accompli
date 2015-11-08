599cb9e03d4fe94bef74ab5cf1f3e0f0858d55cd
Niels Nijens <nijens.niels@gmail.com>
2015-11-08 23:45:37 +0100

    Added ConnectionException

---
+++ src/Exception/ConnectionException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Accompli\Exception;
+
+use RuntimeException;
+
+/**
+ * ConnectionException.
+ *
+ * @author Niels Nijens <nijens.niels@gmail.com>
+ */
+class ConnectionException extends RuntimeException
+{
+}
