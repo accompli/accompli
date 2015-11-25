abd4142748e9c9a99dcf7b500957d083421a2c57
Niels Nijens <nijens.niels@gmail.com>
2015-11-25 17:13:49 +0100

    Added ConnectionAdapterProcessExecutor

---
+++ tests/Deployment/Connection/ConnectionAdapterProcessExecutorTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Accompli\Test;
+
+use Accompli\Chrono\Process\ProcessExecutionResult;
+use Accompli\Deployment\Connection\ConnectionAdapterProcessExecutor;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * ConnectionAdapterProcessExecutorTest.
+ *
+ * @author Niels Nijens <nijens.niels@gmail.com>
+ */
+class ConnectionAdapterProcessExecutorTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests if constructing a new ConnectionAdapterProcessExecutor instance sets the instance properties.
+     */
+    public function testConstruct()
+    {
+        $connectionAdapterMock = $this->getMockBuilder('Accompli\Deployment\Connection\ConnectionAdapterInterface')->getMock();
+
+        $processExecutor = new ConnectionAdapterProcessExecutor($connectionAdapterMock);
+
+        $this->assertAttributeSame($connectionAdapterMock, 'connectionAdapter', $processExecutor);
+    }
+
+    /**
+     * Tests if ConnectionAdapterProcessExecutor::isDirectory returns the expected result.
+     *
+     * @depends testConstruct
+     */
+    public function testIsDirectory()
+    {
+        $connectionAdapterMock = $this->getMockBuilder('Accompli\Deployment\Connection\ConnectionAdapterInterface')->getMock();
+        $connectionAdapterMock->expects($this->once())->method('isDirectory')->willReturn(true);
+
+        $processExecutor = new ConnectionAdapterProcessExecutor($connectionAdapterMock);
+
+        $this->assertTrue($processExecutor->isDirectory('/test/path'));
+    }
+
+    /**
+     * Tests if ConnectionAdapterProcessExecutor::execute returns the expected result.
+     *
+     * @depends testConstruct
+     */
+    public function testExecute()
+    {
+        $connectionAdapterMock = $this->getMockBuilder('Accompli\Deployment\Connection\ConnectionAdapterInterface')->getMock();
+        $connectionAdapterMock->expects($this->once())->method('executeCommand')->willReturn(new ProcessExecutionResult(0, '', ''));
+
+        $processExecutor = new ConnectionAdapterProcessExecutor($connectionAdapterMock);
+
+        $this->assertInstanceOf('Accompli\Chrono\Process\ProcessExecutionResult', $processExecutor->execute('echo test'));
+    }
+
+    /**
+     * Tests if ConnectionAdapterProcessExecutor::execute changes the working directory and returns the expected result.
+     *
+     * @depends testExecute
+     */
+    public function testExecuteWithWorkingDirectory()
+    {
+        $connectionAdapterMock = $this->getMockBuilder('Accompli\Deployment\Connection\ConnectionAdapterInterface')->getMock();
+        $connectionAdapterMock->expects($this->once())
+                ->method('getWorkingDirectory')
+                ->willReturn('/previous/directory');
+        $connectionAdapterMock->expects($this->exactly(2))
+                ->method('changeWorkingDirectory')
+                ->withConsecutive(
+                        array('/test/path'),
+                        array('/previous/directory')
+                )
+                ->willReturn(true);
+        $connectionAdapterMock->expects($this->once())
+                ->method('executeCommand')
+                ->willReturn(new ProcessExecutionResult(0, '', ''));
+
+        $processExecutor = new ConnectionAdapterProcessExecutor($connectionAdapterMock);
+
+        $this->assertInstanceOf('Accompli\Chrono\Process\ProcessExecutionResult', $processExecutor->execute('echo test', '/test/path'));
+    }
+}
