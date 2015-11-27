8d02cb5643b9b38d3e9c893f1aec03ceffce0b40
Niels Nijens <nijens.niels@gmail.com>
2015-11-27 16:30:28 +0100

    Improved PrepareDeployReleaseEvent

--- src/EventDispatcher/Event/PrepareDeployReleaseEvent.php
+++ src/EventDispatcher/Event/PrepareDeployReleaseEvent.php
@@ -2,6 +2,8 @@
 
 namespace Accompli\EventDispatcher\Event;
 
+use Accompli\Deployment\Release;
+
 /**
  * PrepareDeployReleaseEvent.
  *
@@ -9,4 +11,30 @@ namespace Accompli\EventDispatcher\Event;
  */
 class PrepareDeployReleaseEvent extends PrepareReleaseEvent
 {
+    /**
+     * The currently deployed Release instance.
+     *
+     * @var Release
+     */
+    private $currentRelease;
+
+    /**
+     * Returns the currently deployed Release instance.
+     *
+     * @return Release
+     */
+    public function getCurrentRelease()
+    {
+        return $this->currentRelease;
+    }
+
+    /**
+     * Sets the currently deployed Release instance.
+     *
+     * @param Release $release
+     */
+    public function setCurrentRelease(Release $release)
+    {
+        $this->currentRelease = $release;
+    }
 }
