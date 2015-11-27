891406cd0c6eeaea30f3d092b7be017289e177b6
Niels Nijens <nijens.niels@gmail.com>
2015-11-27 16:29:46 +0100

    Improved DeployReleaseEvent and added tests

--- src/EventDispatcher/Event/DeployReleaseEvent.php
+++ src/EventDispatcher/Event/DeployReleaseEvent.php
@@ -2,6 +2,8 @@
 
 namespace Accompli\EventDispatcher\Event;
 
+use Accompli\Deployment\Release;
+
 /**
  * DeployReleaseEvent.
  *
@@ -9,4 +11,33 @@ namespace Accompli\EventDispatcher\Event;
  */
 class DeployReleaseEvent extends ReleaseEvent
 {
+    /**
+     * The currently deployed Release instance.
+     *
+     * @var Release
+     */
+    private $currentRelease;
+
+    /**
+     * Constructs a new DeployReleaseEvent instance.
+     *
+     * @param Release      $release
+     * @param Release|null $currentRelease
+     */
+    public function __construct(Release $release, Release $currentRelease = null)
+    {
+        parent::__construct($release);
+
+        $this->currentRelease = $currentRelease;
+    }
+
+    /**
+     * Returns the currently deployed Release instance.
+     *
+     * @return Release
+     */
+    public function getCurrentRelease()
+    {
+        return $this->currentRelease;
+    }
 }
