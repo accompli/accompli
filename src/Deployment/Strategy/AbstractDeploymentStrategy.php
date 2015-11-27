298b98610c3c82b14744a8f4bb26709c08e7a0d5
Niels Nijens <nijens.niels@gmail.com>
2015-11-27 16:30:57 +0100

    Implemented deployment flow in AbstractDeploymentStrategy::deploy

--- src/Deployment/Strategy/AbstractDeploymentStrategy.php
+++ src/Deployment/Strategy/AbstractDeploymentStrategy.php
@@ -2,10 +2,19 @@
 
 namespace Accompli\Deployment\Strategy;
 
+use Accompli\AccompliEvents;
 use Accompli\Configuration\ConfigurationInterface;
 use Accompli\DependencyInjection\ConfigurationAwareInterface;
 use Accompli\DependencyInjection\EventDispatcherAwareInterface;
+use Accompli\Deployment\Release;
+use Accompli\Deployment\Workspace;
+use Accompli\EventDispatcher\Event\DeployReleaseEvent;
+use Accompli\EventDispatcher\Event\FailedEvent;
+use Accompli\EventDispatcher\Event\HostEvent;
+use Accompli\EventDispatcher\Event\PrepareDeployReleaseEvent;
+use Accompli\EventDispatcher\Event\WorkspaceEvent;
 use Accompli\EventDispatcher\EventDispatcherInterface;
+use Exception;
 
 /**
  * AbstractDeploymentStrategy.
@@ -49,5 +58,36 @@ abstract class AbstractDeploymentStrategy implements DeploymentStrategyInterface
      */
     public function deploy($version, $stage)
     {
+        $hosts = $this->configuration->getHostsByStage($stage);
+        foreach ($hosts as $host) {
+            $exception = null;
+
+            try {
+                $this->eventDispatcher->dispatch(AccompliEvents::CREATE_CONNECTION, new HostEvent($host));
+
+                $workspaceEvent = new WorkspaceEvent($host);
+                $this->eventDispatcher->dispatch(AccompliEvents::GET_WORKSPACE, $workspaceEvent);
+
+                $workspace = $workspaceEvent->getWorkspace();
+                if ($workspace instanceof Workspace) {
+                    $prepareDeployReleaseEvent = new PrepareDeployReleaseEvent($workspace, $version);
+                    $this->eventDispatcher->dispatch(AccompliEvents::PREPARE_DEPLOY_RELEASE, $prepareDeployReleaseEvent);
+
+                    $release = $prepareDeployReleaseEvent->getRelease();
+                    if ($release instanceof Release) {
+                        $deployReleaseEvent = new DeployReleaseEvent($release, $prepareDeployReleaseEvent->getCurrentRelease());
+                        $this->eventDispatcher->dispatch(AccompliEvents::DEPLOY_RELEASE, $deployReleaseEvent);
+
+                        $this->eventDispatcher->dispatch(AccompliEvents::DEPLOY_RELEASE_COMPLETE, $deployReleaseEvent);
+
+                        continue;
+                    }
+                }
+            } catch (Exception $exception) {
+            }
+
+            $failedEvent = new FailedEvent($this->eventDispatcher->getLastDispatchedEvent(), $exception);
+            $this->eventDispatcher->dispatch(AccompliEvents::DEPLOY_RELEASE_FAILED, $failedEvent);
+        }
     }
 }
--- tests/Deployment/Strategy/AbstractDeploymentStrategyTest.php
+++ tests/Deployment/Strategy/AbstractDeploymentStrategyTest.php
@@ -2,7 +2,15 @@
 
 namespace Accompli\Test;
 
+use Accompli\AccompliEvents;
+use Accompli\Deployment\Host;
+use Accompli\EventDispatcher\Event\DeployReleaseEvent;
+use Accompli\EventDispatcher\Event\FailedEvent;
+use Accompli\EventDispatcher\Event\HostEvent;
+use Accompli\EventDispatcher\Event\PrepareDeployReleaseEvent;
+use Accompli\EventDispatcher\Event\WorkspaceEvent;
 use PHPUnit_Framework_TestCase;
+use Symfony\Component\EventDispatcher\Event;
 
 /**
  * AbstractDeploymentStrategyTest.
@@ -35,5 +43,288 @@ class AbstractDeploymentStrategyTest extends PHPUnit_Framework_TestCase
         $deploymentStrategyMock->setEventDispatcher($eventDispatcherMock);
 
         $this->assertAttributeSame($eventDispatcherMock, 'eventDispatcher', $deploymentStrategyMock);
+    }
+
+    /**
+     * Tests if AbstractDeploymentStrategy::deploy dispatches all the events successfully.
+     *
+     * @depends testSetConfiguration
+     * @depends testSetEventDispatcher
+     */
+    public function testDeployDispatchesEventsSuccessfully()
+    {
+        $hostMock = $this->getMockBuilder('Accompli\Deployment\Host')
+                ->setConstructorArgs(array(Host::STAGE_TEST, 'local', null, __DIR__))
+                ->getMock();
+
+        $configurationMock = $this->getMockBuilder('Accompli\Configuration\ConfigurationInterface')->getMock();
+        $configurationMock->expects($this->once())
+                ->method('getHostsByStage')
+                ->with($this->equalTo(Host::STAGE_TEST))
+                ->willReturn(array($hostMock));
+
+        $eventDispatcherMock = $this->getMockBuilder('Accompli\EventDispatcher\EventDispatcherInterface')->getMock();
+        $eventDispatcherMock->expects($this->exactly(5))
+                ->method('dispatch')
+                ->withConsecutive(
+                    array(
+                        $this->equalTo(AccompliEvents::CREATE_CONNECTION),
+                        $this->callback(function ($event) {
+                            return ($event instanceof HostEvent);
+                        }),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::GET_WORKSPACE),
+                        $this->callback(array($this, 'provideDispatchCallbackForWorkspaceEvent')),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::PREPARE_DEPLOY_RELEASE),
+                        $this->callback(array($this, 'provideDispatchCallbackForPrepareDeployReleaseEvent')),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::DEPLOY_RELEASE),
+                        $this->callback(function ($event) {
+                            return ($event instanceof DeployReleaseEvent);
+                        }),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::DEPLOY_RELEASE_COMPLETE),
+                        $this->callback(function ($event) {
+                            return ($event instanceof DeployReleaseEvent);
+                        }),
+                    )
+                );
+
+        $strategyMock = $this->getMockBuilder('Accompli\Deployment\Strategy\AbstractDeploymentStrategy')->getMockForAbstractClass();
+        $strategyMock->setConfiguration($configurationMock);
+        $strategyMock->setEventDispatcher($eventDispatcherMock);
+        $strategyMock->deploy('0.1.0', Host::STAGE_TEST);
+    }
+
+    /**
+     * Tests if AbstractDeploymentStrategy::deploy dispatches all the events successfully for multiple hosts.
+     *
+     * @depends testDeployDispatchesEventsSuccessfully
+     */
+    public function testDeployDispatchesEventsSuccessfullyForMultipleHosts()
+    {
+        $hostMock = $this->getMockBuilder('Accompli\Deployment\Host')
+                ->setConstructorArgs(array(Host::STAGE_TEST, 'local', null, __DIR__))
+                ->getMock();
+
+        $configurationMock = $this->getMockBuilder('Accompli\Configuration\ConfigurationInterface')->getMock();
+        $configurationMock->expects($this->once())
+                ->method('getHostsByStage')
+                ->with($this->equalTo(Host::STAGE_TEST))
+                ->willReturn(array($hostMock, $hostMock));
+
+        $eventDispatcherMock = $this->getMockBuilder('Accompli\EventDispatcher\EventDispatcherInterface')->getMock();
+        $eventDispatcherMock->expects($this->exactly(10))
+                ->method('dispatch')
+                ->withConsecutive(
+                    array(
+                        $this->equalTo(AccompliEvents::CREATE_CONNECTION),
+                        $this->callback(function ($event) {
+                            return ($event instanceof HostEvent);
+                        }),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::GET_WORKSPACE),
+                        $this->callback(array($this, 'provideDispatchCallbackForWorkspaceEvent')),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::PREPARE_DEPLOY_RELEASE),
+                        $this->callback(array($this, 'provideDispatchCallbackForPrepareDeployReleaseEvent')),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::DEPLOY_RELEASE),
+                        $this->callback(function ($event) {
+                            return ($event instanceof DeployReleaseEvent);
+                        }),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::DEPLOY_RELEASE_COMPLETE),
+                        $this->callback(function ($event) {
+                            return ($event instanceof DeployReleaseEvent);
+                        }),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::CREATE_CONNECTION),
+                        $this->callback(function ($event) {
+                            return ($event instanceof HostEvent);
+                        }),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::GET_WORKSPACE),
+                        $this->callback(array($this, 'provideDispatchCallbackForWorkspaceEvent')),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::PREPARE_DEPLOY_RELEASE),
+                        $this->callback(array($this, 'provideDispatchCallbackForPrepareDeployReleaseEvent')),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::DEPLOY_RELEASE),
+                        $this->callback(function ($event) {
+                            return ($event instanceof DeployReleaseEvent);
+                        }),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::DEPLOY_RELEASE_COMPLETE),
+                        $this->callback(function ($event) {
+                            return ($event instanceof DeployReleaseEvent);
+                        }),
+                    )
+                );
+
+        $strategyMock = $this->getMockBuilder('Accompli\Deployment\Strategy\AbstractDeploymentStrategy')->getMockForAbstractClass();
+        $strategyMock->setConfiguration($configurationMock);
+        $strategyMock->setEventDispatcher($eventDispatcherMock);
+        $strategyMock->deploy('0.1.0', Host::STAGE_TEST);
+    }
+
+    /**
+     * Tests if AbstractDeploymentStrategy::deploy dispatches all the events untill after the WorkspaceEvent.
+     *
+     * @depends testDeployDispatchesEventsSuccessfully
+     */
+    public function testDeployDispatchesEventsSuccessfullyUntillAfterWorkspaceEvent()
+    {
+        $hostMock = $this->getMockBuilder('Accompli\Deployment\Host')
+                ->setConstructorArgs(array(Host::STAGE_TEST, 'local', null, __DIR__))
+                ->getMock();
+
+        $configurationMock = $this->getMockBuilder('Accompli\Configuration\ConfigurationInterface')->getMock();
+        $configurationMock->expects($this->once())
+                ->method('getHostsByStage')
+                ->with($this->equalTo(Host::STAGE_TEST))
+                ->willReturn(array($hostMock));
+
+        $eventDispatcherMock = $this->getMockBuilder('Accompli\EventDispatcher\EventDispatcherInterface')->getMock();
+        $eventDispatcherMock->expects($this->once())
+                ->method('getLastDispatchedEvent')
+                ->willReturn(new Event());
+
+        $eventDispatcherMock->expects($this->exactly(3))
+                ->method('dispatch')
+                ->withConsecutive(
+                    array(
+                        $this->equalTo(AccompliEvents::CREATE_CONNECTION),
+                        $this->callback(function ($event) {
+                            return ($event instanceof HostEvent);
+                        }),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::GET_WORKSPACE),
+                        $this->callback(function ($event) {
+                            return ($event instanceof WorkspaceEvent);
+                        }),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::DEPLOY_RELEASE_FAILED),
+                        $this->callback(function ($event) {
+                            return ($event instanceof FailedEvent);
+                        }),
+                    )
+                );
+
+        $strategyMock = $this->getMockBuilder('Accompli\Deployment\Strategy\AbstractDeploymentStrategy')->getMockForAbstractClass();
+        $strategyMock->setConfiguration($configurationMock);
+        $strategyMock->setEventDispatcher($eventDispatcherMock);
+        $strategyMock->deploy('0.1.0', Host::STAGE_TEST);
+    }
+
+    /**
+     * Tests if AbstractDeploymentStrategy::deploy dispatches all the events untill after the PrepareDeployReleaseEvent.
+     *
+     * @depends testDeployDispatchesEventsSuccessfully
+     */
+    public function testDeployDispatchesEventsSuccessfullyUntillAfterPrepareDeployReleaseEvent()
+    {
+        $hostMock = $this->getMockBuilder('Accompli\Deployment\Host')
+                ->setConstructorArgs(array(Host::STAGE_TEST, 'local', null, __DIR__))
+                ->getMock();
+
+        $configurationMock = $this->getMockBuilder('Accompli\Configuration\ConfigurationInterface')->getMock();
+        $configurationMock->expects($this->once())
+                ->method('getHostsByStage')
+                ->with($this->equalTo(Host::STAGE_TEST))
+                ->willReturn(array($hostMock));
+
+        $eventDispatcherMock = $this->getMockBuilder('Accompli\EventDispatcher\EventDispatcherInterface')->getMock();
+        $eventDispatcherMock->expects($this->once())
+                ->method('getLastDispatchedEvent')
+                ->willReturn(new Event());
+
+        $eventDispatcherMock->expects($this->exactly(4))
+                ->method('dispatch')
+                ->withConsecutive(
+                    array(
+                        $this->equalTo(AccompliEvents::CREATE_CONNECTION),
+                        $this->callback(function ($event) {
+                            return ($event instanceof HostEvent);
+                        }),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::GET_WORKSPACE),
+                        $this->callback(array($this, 'provideDispatchCallbackForWorkspaceEvent')),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::PREPARE_DEPLOY_RELEASE),
+                        $this->callback(function ($event) {
+                            return ($event instanceof PrepareDeployReleaseEvent);
+                        }),
+                    ),
+                    array(
+                        $this->equalTo(AccompliEvents::DEPLOY_RELEASE_FAILED),
+                        $this->callback(function ($event) {
+                            return ($event instanceof FailedEvent);
+                        }),
+                    )
+                );
+
+        $strategyMock = $this->getMockBuilder('Accompli\Deployment\Strategy\AbstractDeploymentStrategy')->getMockForAbstractClass();
+        $strategyMock->setConfiguration($configurationMock);
+        $strategyMock->setEventDispatcher($eventDispatcherMock);
+        $strategyMock->deploy('0.1.0', Host::STAGE_TEST);
+    }
+
+    /**
+     * Provides the dispatch test callback for the WorkspaceEvent.
+     *
+     * @see testDeployDispatchesEventsSuccessfully
+     *
+     * @param Event $event
+     *
+     * @return bool
+     */
+    public function provideDispatchCallbackForWorkspaceEvent(Event $event)
+    {
+        $workspaceMock = $this->getMockBuilder('Accompli\Deployment\Workspace')
+                ->setConstructorArgs(array($event->getHost()))
+                ->getMock();
+
+        $event->setWorkspace($workspaceMock);
+
+        return ($event instanceof WorkspaceEvent);
+    }
+
+    /**
+     * Provides the dispatch test callback for the PrepareDeployReleaseEvent.
+     *
+     * @see testDeployDispatchesEventsSuccessfully
+     *
+     * @param Event $event
+     *
+     * @return bool
+     */
+    public function provideDispatchCallbackForPrepareDeployReleaseEvent(Event $event)
+    {
+        $releaseMock = $this->getMockBuilder('Accompli\Deployment\Release')
+                ->setConstructorArgs(array($event->getVersion()))
+                ->getMock();
+
+        $event->setRelease($releaseMock);
+
+        return ($event instanceof PrepareDeployReleaseEvent);
     }
 }
