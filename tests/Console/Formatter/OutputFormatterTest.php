3c0090bc415f044050d4593b87c4aae49c8ecef6
Niels Nijens <nijens.niels@gmail.com>
2015-12-14 18:26:50 +0100

    Added styles for all PSR log levels to OutputFormatter

--- tests/Console/Formatter/OutputFormatterTest.php
+++ tests/Console/Formatter/OutputFormatterTest.php
@@ -4,6 +4,7 @@ namespace Accompli\Test;
 
 use Accompli\Console\Formatter\OutputFormatter;
 use PHPUnit_Framework_TestCase;
+use Psr\Log\LogLevel;
 
 /**
  * OutputFormatterTest.
@@ -14,15 +15,39 @@ class OutputFormatterTest extends PHPUnit_Framework_TestCase
 {
     /**
      * Tests if constructing a new OutputFormatter sets the styles.
+     *
+     * @dataProvider provideTestConstruct
+     *
+     * @param string $styleName
      */
-    public function testConstruct()
+    public function testConstruct($styleName)
     {
         $formatter = new OutputFormatter();
 
-        $this->assertTrue($formatter->hasStyle('event-name'));
-        $this->assertTrue($formatter->hasStyle('event-task-name'));
-        $this->assertTrue($formatter->hasStyle('event-task-action-in_progress'));
-        $this->assertTrue($formatter->hasStyle('event-task-action-failed'));
-        $this->assertTrue($formatter->hasStyle('event-task-action-completed'));
+        $this->assertTrue($formatter->hasStyle($styleName));
+    }
+
+    /**
+     * Returns an array with test cases for @see testConstruct.
+     *
+     * @return array
+     */
+    public function provideTestConstruct()
+    {
+        return array(
+            array(LogLevel::EMERGENCY),
+            array(LogLevel::CRITICAL),
+            array(LogLevel::ALERT),
+            array(LogLevel::ERROR),
+            array(LogLevel::WARNING),
+            array(LogLevel::NOTICE),
+            array(LogLevel::INFO),
+            array(LogLevel::DEBUG),
+            array('event-name'),
+            array('event-task-name'),
+            array('event-task-action-in_progress'),
+            array('event-task-action-failed'),
+            array('event-task-action-completed'),
+        );
     }
 }
