eb147e08ecb973b8f3f297be1454183bfe83e227
Niels Nijens <nijens.niels@gmail.com>
2016-05-29 11:42:13 +0200

    Improve CommandTester flexibility by providing input and output instances and setting an input stream

--- tests/Console/Command/CommandTester.php
+++ tests/Console/Command/CommandTester.php
@@ -2,7 +2,9 @@
 
 namespace Accompli\Test\Console\Command;
 
+use ReflectionClass;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,22 +14,70 @@ use Symfony\Component\Console\Output\StreamOutput;
  * Eases the testing of console commands.
  *
  * @author Fabien Potencier <fabien@symfony.com>
+ * @author Niels Nijens <nijens.niels@gmail.com>
  */
 class CommandTester
 {
+    /**
+     * The Command instance.
+     *
+     * @var Command
+     */
     private $command;
+
+    /**
+     * The InputInterface instance.
+     *
+     * @var InputInterface
+     */
     private $input;
+
+    /**
+     * The OutputInterface instance.
+     *
+     * @var OutputInterface
+     */
     private $output;
+
+    /**
+     * The exit code of the command.
+     *
+     * @var int
+     */
     private $statusCode;
 
     /**
-     * Constructor.
+     * The input stream resource.
+     *
+     * @var resource
+     */
+    private $inputStream;
+
+    /**
+     * Constructs a new CommandTester instance.
      *
      * @param Command $command A Command instance to test.
      */
     public function __construct(Command $command)
     {
         $this->command = $command;
+    }
+
+    /**
+     * Sets the string to an input stream.
+     *
+     * @param string $input
+     */
+    public function setInputStream($input)
+    {
+        $this->inputStream = fopen('php://memory', 'r+', false);
+        fwrite($this->inputStream, $input);
+        rewind($this->inputStream);
+
+        $questionHelper = $this->command->getHelperSet()->get('question');
+        if ($questionHelper instanceof QuestionHelper) {
+            $questionHelper->setInputStream($this->inputStream);
+        }
     }
 
     /**
@@ -39,28 +89,36 @@ class CommandTester
      *  * decorated:   Sets the output decorated flag
      *  * verbosity:   Sets the output verbosity flag
      *
-     * @param array $input   An array of command arguments and options
-     * @param array $options An array of execution options
+     * @param InputInterface|array $input   An InputInterface instance or array of command arguments and options
+     * @param array                $options An array of execution options
+     * @param StreamOutput|null    $output  An StreamOutput instance
      *
      * @return int The command exit code
      */
-    public function execute(array $input, array $options = array())
+    public function execute($input, array $options = array(), StreamOutput $output = null)
     {
         // set the command name automatically if the application requires
         // this argument and no command name was passed
-        if (!isset($input['command'])
+        if (is_array($input)
+            && !isset($input['command'])
             && (null !== $application = $this->command->getApplication())
             && $application->getDefinition()->hasArgument('command')
         ) {
             $input = array_merge(array('command' => $this->command->getName()), $input);
         }
 
-        $this->input = new ArrayInput($input);
+        $this->input = $input;
+        if ($this->input instanceof InputInterface === false) {
+            $this->input = new ArrayInput($this->input);
+        }
         if (isset($options['interactive'])) {
             $this->input->setInteractive($options['interactive']);
         }
 
-        $this->output = new StreamOutput(fopen('php://memory', 'w', false));
+        $this->output = $output;
+        if ($this->output instanceof StreamOutput === false) {
+            $this->output = new StreamOutput(fopen('php://memory', 'w', false));
+        }
         if (isset($options['decorated'])) {
             $this->output->setDecorated($options['decorated']);
         }
@@ -99,6 +157,16 @@ class CommandTester
     public function getInput()
     {
         return $this->input;
+    }
+
+    /**
+     * Returns the input stream resource.
+     *
+     * @return resource
+     */
+    public function getInputStream()
+    {
+        return $this->inputStream;
     }
 
     /**
