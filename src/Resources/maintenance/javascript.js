881522af97d4247b03e2a89ccf94f71b9b724604
deborahvandervegt <deborah@connectholland.nl>
2016-06-28 16:24:51 +0200

    Remove javascript file, add javascript in index html

--- src/Resources/maintenance/javascript.js
+++
@@ -1,8 +0,0 @@
-var check = setInterval(function(){ checkForMaintentance() }, 30000);
-
-function checkForMaintentance() {
-    if (document.title.indexOf("maintenance") == -1) {
-        clearInterval(check);
-        location.reload(true);
-    }
-}
