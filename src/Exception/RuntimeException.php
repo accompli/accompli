0c9aaa430077804588f238e86b81f402def5b021
Niels Nijens <nijens.niels@gmail.com>
2016-07-12 23:51:22 +0200

    Add RuntimeException

---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Accompli\Exception;
+
+use RuntimeException as BaseRuntimeException;
+
+/**
+ * RuntimeException.
+ *
+ * @author Niels Nijens <nijens.niels@gmail.com>
+ */
+class RuntimeException extends BaseRuntimeException
+{
+}
